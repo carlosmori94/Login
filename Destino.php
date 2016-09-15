78635a4cff1021ea06a9f6335832f50febb59e44
carlosmori94 <carlosmori34@gmail.com>
2016-09-15 10:54:03 -0300

    Login completo

---
+++ Destino.php
@@ -0,0 +1,8 @@
+<?php
+$user = $_POST["user"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+echo "Bienvenido ".$user."<BR>Tu contraseña es: ".$password."<BR>Tu email es: ".$email;
+
+?>
---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form id="primerFormulario" action="Destino.php" method="POST">
+		<table align="center">
+			
+				<tr><td colspan="2" align="center">Login</td></tr>
+
+					<tr><td>Usuario:            </td><td><input type="text" name="user">        </td></tr>
+					<tr><td>Contraseña:         </td><td><input type="password" name="password"></td></tr>
+					<tr><td>Correo Electronico: </td><td><input type="text" name="email">       </td></tr>
+						<tr><td colspan="2" align="center"><input type="submit" name="Entrar"></td></tr>
+		</table>
+	</form>
+
+</body>
+</html>
